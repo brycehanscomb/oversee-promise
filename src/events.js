839795324f34628a1daa540045fef8aeb7561495
Bryce Hanscomb <b.hanscomb@gmail.com>
2016-08-02 17:50:27 +1000

    refactor: change events enumeration to use individually exported values

--- src/events.js
+++ src/events.js
@@ -1,8 +1,6 @@
-export default {
-    BEGIN_EXECUTING: 'BEGIN_EXECUTING',
-    COMPLETED_SUCCESSFULLY: 'COMPLETED_SUCCESSFULLY',
-    COMPLETED_UNSUCCESSFULLY: 'COMPLETED_UNSUCCESSFULLY',
-    MESSAGE_CHANGED: 'MESSAGE_CHANGED',
-    STATE_CHANGED: 'STATE_CHANGED',
-    RESULT_CHANGED: 'RESULT_CHANGED'
-};
+export const BEGIN_EXECUTING = 'begin_executing';
+export const COMPLETED_SUCCESSFULLY = 'completed_successfully';
+export const COMPLETED_UNSUCCESSFULLY = 'completed_unsuccessfully';
+export const MESSAGE_CHANGED = 'message_changed';
+export const STATE_CHANGED = 'state_changed';
+export const RESULT_CHANGED = 'result_changed';
